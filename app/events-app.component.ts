426b6d7c1bec45a5da91d9a1254d43602b221e0e
matiasstrizzolo <matiasstrizzolo@gmail.com>
2018-03-22 06:27:03 -0300

    convert dir into git repo

---
+++ app/events-app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'events-app',
+    template: `
+    <div>
+        <nav-bar></nav-bar>
+        <events-list></events-list>
+    </div>
+    `
+})
+export class EventsAppComponent {
+
+}
---
+++ app/events/event-thumbnail.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+        <div class="well hoverwell thumbnail">
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+                <span *ngSwitchDefault>(Normal start)</span>
+            </div>
+            <div>Price: \${{event?.price}}</div>
+            <div *ngIf="event?.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div [hidden]="!event?.onlineUrl">
+                Online Url: {{event?.onlineUrl}}
+            </div>
+        </div>
+    `,
+    styles: [`
+        .green { color: #003300 !important }
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb }
+    `]
+})
+export class EventThumbnailComponent {
+    @Input() event: any;
+    @Output() eventClick = new EventEmitter();
+    someProp: any = 'local variable'
+
+    handleClickMe() {
+        this.eventClick.emit(this.event.name);
+    }
+
+    logFoo() {
+        console.log('foo');
+    }
+}
